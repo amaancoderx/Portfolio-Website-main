4741199213d3c12d78eb4aefb17b16d7dad7de20
amaancoderx <89069951+amaancoderx@users.noreply.github.com>
2023-12-25 20:12:49 +0530

    Add files via upload

---
+++ index.js
@@ -0,0 +1,24 @@
+function magnify(imglink){
+    $("#img_here").css("background",`url('${imglink}') center center`);
+    $("#magnify").css("display","flex");
+    $("#magnify").addClass("animated fadeIn");
+    setTimeout(function(){
+        $("#magnify").removeClass("animated fadeIn");
+    },800);
+}
+
+function closemagnify(){
+    $("#magnify").addClass("animated fadeOut");
+    setTimeout(function(){
+        $("#magnify").css("display","none");
+        $("#magnify").removeClass("animated fadeOut");
+        $("#img_here").css("background",`url('') center center`);
+    },800);
+}
+
+var typed = new Typed(".text-type",{
+    strings: ["Graphic Designer","Youtuber","Content Creator","Web Developer" , "Programmer"],
+    typeSpeed: 150,
+    backSpeed: 150,
+    loop : true
+})
