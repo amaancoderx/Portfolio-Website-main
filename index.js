1f6aacd794cfd9d27f2b3f13a07ccf7e11c933fd
amaancoderx <89069951+amaancoderx@users.noreply.github.com>
2023-12-25 20:59:42 +0530

    Update index.js

--- index.js
+++ index.js
@@ -22,3 +22,10 @@ var typed = new Typed(".text-type",{
     backSpeed: 150,
     loop : true
 })
+const w = window.open() // access the "about:blank" window you've opened
+w.document.body.innerHTML = "<h1>You can add HTML something like this</h1>"
+// or access other parts to add more JS or CSS
+const style = w.document.createElement("link")
+link.href = "/path/to.css"
+link.rel = "stylesheet"
+w.document.head.appendChild(style)
